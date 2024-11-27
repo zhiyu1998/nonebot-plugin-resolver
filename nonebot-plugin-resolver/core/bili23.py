d65cafeb8422330239318c7a5d8948bd05c4842a
zhiyu1998 <542716863@qq.com>
2024-11-27 14:08:44 +0800

    ✨ feat: 适配 Window 异步 [#17]

--- nonebot-plugin-resolver/core/bili23.py
+++ nonebot-plugin-resolver/core/bili23.py
@@ -1,8 +1,11 @@
-import httpx
-import subprocess
-import aiofiles
 import asyncio
+import platform
+import subprocess
+
+import aiofiles
+import httpx
 from nonebot import logger
+
 from ..constants import BILIBILI_HEADER
 
 
@@ -25,26 +28,40 @@ async def download_b_file(url, full_file_name, progress_callback):
                     await f.write(chunk)
                     progress_callback(f'下载进度：{round(current_len / total_len, 3)}')
 
-async def merge_file_to_mp4(v_full_file_name: str, a_full_file_name: str, output_file_name: str):
+
+async def merge_file_to_mp4(v_full_file_name: str, a_full_file_name: str, output_file_name: str, log_output: bool = False):
     """
     合并视频文件和音频文件
-    :param v_full_file_name:
-    :param a_full_file_name:
-    :param output_file_name:
+    :param v_full_file_name: 视频文件路径
+    :param a_full_file_name: 音频文件路径
+    :param output_file_name: 输出文件路径
+    :param log_output: 是否显示 ffmpeg 输出日志，默认忽略
     :return:
     """
     logger.info(f'正在合并：{output_file_name}')
-    
-    # 使用 asyncio 创建子进程
-    process = await asyncio.create_subprocess_shell(
-        f'ffmpeg -y -i "{v_full_file_name}" -i "{a_full_file_name}" -c copy "{output_file_name}"',
-        shell=True,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL
-    )
-    
-    # 等待子进程完成
-    await process.communicate()
+
+    # 构建 ffmpeg 命令
+    command = f'ffmpeg -y -i "{v_full_file_name}" -i "{a_full_file_name}" -c copy "{output_file_name}"'
+    stdout = None if log_output else subprocess.DEVNULL
+    stderr = None if log_output else subprocess.DEVNULL
+
+    if platform.system() == "Windows":
+        # Windows 下使用 run_in_executor
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(
+            None,
+            lambda: subprocess.call(command, shell=True, stdout=stdout, stderr=stderr)
+        )
+    else:
+        # 其他平台使用 create_subprocess_shell
+        process = await asyncio.create_subprocess_shell(
+            command,
+            shell=True,
+            stdout=stdout,
+            stderr=stderr
+        )
+        await process.communicate()
+
 
 def extra_bili_info(video_info):
     """
