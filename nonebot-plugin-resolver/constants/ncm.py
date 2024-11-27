a0cd65a51eb7bdd6de8a55eb2f8346d4b34a732d
Priopus <134188283+Priopus@users.noreply.github.com>
2024-11-27 15:54:14 +0800

    feat: 修改最高音质接口

--- nonebot-plugin-resolver/constants/ncm.py
+++ nonebot-plugin-resolver/constants/ncm.py
@@ -6,4 +6,4 @@ NETEASE_API_CN = 'https://www.markingchen.ink'
 """
 NCM临时接口
 """
-NETEASE_TEMP_API = "https://api.lolimi.cn/API/wydg/api.php?msg={}&n=1"
+NETEASE_TEMP_API = "https://www.hhlqilongzhu.cn/api/dg_wyymusic.php?id={}&br=7&type=json"
